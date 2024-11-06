673046689430fd61315a9d22931f543cb8c3ab0f
ikedahaj <132437214+ikedahaj@users.noreply.github.com>
2024-11-06 15:53:30 +0900

    モジュールの表示方法を変更

--- UI_input_doi.py
+++ UI_input_doi.py
@@ -1,5 +1,5 @@
 # doiを入力する画面;
-
+import time
 import typing
 import sys
 
@@ -9,6 +9,7 @@ import configparser
 import papnt
 
 import papnt.misc
+import papnt.database
 
 # doiからnotionに論文情報を追加する;
 def __create_records_from_doi(doi:str):
@@ -43,28 +44,39 @@ def __format_doi(doi:str)->str:
 class _Edit_Database(ft.Row):
     def __init__(self):
         super().__init__()
-        self.ED_path_config=papnt.__path__[0]+"/config.ini"
+        self.__ED_path_config=papnt.__path__[0]+"/config.ini"
         self.config=configparser.ConfigParser(comment_prefixes='/', allow_no_value=True)
-        self.config.read(self.ED_path_config)
-        self.ED_text_tokenkey=ft.Text(value=(None if self.config["database"]["tokenkey"] =="''" else self.config["database"]["tokenkey"]) )
-        self.ED_text_database_id=ft.Text(value=(None if self.config["database"]["database_id"]=="''" else self.config["database"]["database_id"]))
-        ED_buttun_edit=ft.FloatingActionButton(icon=ft.icons.EDIT,on_click=self.__ED_clicked_text_edit)
-        ED_buttun_edit.mini=True
-        self.controls=[ED_buttun_edit,self.ED_text_tokenkey,self.ED_text_database_id]
+        self.config.read(self.__ED_path_config)
+        self.__ED_text_tokenkey=ft.Text(value=(None if self.config["database"]["tokenkey"] =="''" else self.config["database"]["tokenkey"]) )
+        self.__ED_text_database_id=ft.Text(value=(None if self.config["database"]["database_id"]=="''" else self.config["database"]["database_id"]))
+        self.__ED_buttun_edit=ft.FloatingActionButton(icon=ft.icons.EDIT,on_click=self.__ED_clicked_text_edit)
+        self.__ED_buttun_edit.mini=True
+        self.__ED_text_input_database_id=ft.TextField(value=self.__ED_text_database_id.value,hint_text="database_id",visible=False)
+        self.__ED_text_input_tokenkey=ft.TextField(value=self.__ED_text_tokenkey.value,hint_text="tokenkey",visible=False)
+        self.__ED_buttun_done_edit=ft.FloatingActionButton(icon=ft.icons.DONE,on_click=self.__ED_clicked_done_edit,visible=False)
+        self.controls=[self.__ED_buttun_edit,self.__ED_text_tokenkey,self.__ED_text_database_id]
     def __ED_clicked_text_edit(self,e):
-        self.ED_text_database_id=ft.TextField(value=self.config["database"]["database_id"],hint_text="database_id")
-        self.ED_text_tokenkey=ft.TextField(value=self.config["database"]["tokenkey"],hint_text="tokenkey")
-        self.controls=[ft.FloatingActionButton(icon=ft.icons.DONE,on_click=self.__ED_clicked_done_edit),self.ED_text_tokenkey,self.ED_text_database_id]
+        self.__ED_text_input_database_id.visible=True
+        self.__ED_text_input_tokenkey.visible=True
+        self.__ED_buttun_done_edit.visible=True
+        self.__ED_text_database_id.visible=False
+        self.__ED_text_tokenkey.visible=False
+        self.__ED_buttun_edit.visible=False
         self.update()
 
     def __ED_clicked_done_edit(self,e):
-        self.controls[0]=ft.FloatingActionButton(icon=ft.icons.EDIT,on_click=self.__ED_clicked_text_edit,mini=True)
-        self.config["database"]["database_id"]=self.ED_text_database_id.value
-        self.config["database"]["tokenkey"]=self.ED_text_tokenkey.value
-        with open(self.ED_path_config, "w") as configfile:
+        self.__ED_text_database_id.value=self.__ED_text_input_database_id.value
+        self.__ED_text_tokenkey.value=self.__ED_text_input_tokenkey.value
+        self.__ED_text_input_database_id.visible=False
+        self.__ED_text_input_tokenkey.visible=False
+        self.__ED_buttun_done_edit.visible=False
+        self.__ED_text_database_id.visible=True
+        self.__ED_text_tokenkey.visible=True
+        self.__ED_buttun_edit.visible=True
+        self.config["database"]["database_id"]=self.__ED_text_database_id.value
+        self.config["database"]["tokenkey"]=self.__ED_text_tokenkey.value
+        with open(self.__ED_path_config, "w") as configfile:
             self.config.write(configfile, True)
-        self.ED_text_tokenkey=ft.Text(value=self.config["database"]["tokenkey"])
-        self.ED_text_database_id=ft.Text(value=self.config["database"]["database_id"])
         self.update()
 
 # 編集可能なテキスト。
@@ -76,22 +88,20 @@ class _Editable_Text(ft.Row):
     def __init__(self,input_value:str):
         super().__init__()
         self.value=input_value
-        self.ET_text=ft.Text()
-        self.ET_text.value=input_value
-        ET_button_edit=ft.IconButton(icon=ft.icons.EDIT,on_click=self.__ET_clicked_text_edit)
-        ET_button_run=ft.IconButton(icon=ft.icons.RUN_CIRCLE,on_click=self.__ET_clicked_run_papnt)
-        ET_button_delete=ft.IconButton(icon=ft.icons.DELETE,on_click=self.__ET_clicked_text_delete)
-        self.controls=[self.ET_text,ET_button_edit,ET_button_run,ET_button_delete]
+        self.__ET_text=ft.Text()
+        self.__ET_text.value=input_value
+        __ET_button_edit=ft.IconButton(icon=ft.icons.EDIT,on_click=self.__ET_clicked_text_edit,icon_size=20)
+        __ET_button_run=ft.IconButton(icon=ft.icons.RUN_CIRCLE,on_click=self.__ET_clicked_run_papnt,icon_size=20)
+        __ET_button_delete=ft.IconButton(icon=ft.icons.DELETE,on_click=self.__ET_clicked_text_delete,icon_size=20)
+        self.__ET_buttons_plain_text=ft.Row(controls=[__ET_button_edit,__ET_button_run,__ET_button_delete])
+        self.__ET_button_done_edit=ft.FloatingActionButton(icon=ft.icons.DONE,on_click=self.__ET_clicked_done_edit,visible=False)
+        self.__ET_text_input=ft.TextField(value=self.value,on_submit=self.__ET_clicked_done_edit,visible=False)
+        self.controls=[self.__ET_text,self.__ET_buttons_plain_text,self.__ET_text_input,self.__ET_button_done_edit]
     def __ET_clicked_text_edit(self,e):
-        control_save=self.controls
-        ET_button_done_edit=ft.FloatingActionButton(icon=ft.icons.DONE,on_click=self.__ET_clicked_done_edit)
-        new_value=ft.TextField(value=self.value)
-        new_value.on_submit=self.__ET_clicked_done_edit
-        ET_edit_view=[new_value,ET_button_done_edit]
-        self.controls=ET_edit_view
-        self.data=control_save
-        self.update()
-        new_value.focus()
+        self.__ET_text.visible=False
+        self.__ET_buttons_plain_text.visible=False
+        self.__ET_text_input.visible=True
+        self.__ET_button_done_edit.visible=True
     def __ET_clicked_text_delete(self,e):
         self.clean()
 
@@ -99,19 +109,19 @@ class _Editable_Text(ft.Row):
         _run_papnt_doi(self)
 
     def __ET_clicked_done_edit(self,e):
-        saved=self.data
-        self.value=self.controls[0].value
-        # saved[0].value=self.controls[0].value
-        saved[0].value=self.controls[0].value
-        self.controls=saved
-        # self.controls[0].value=self.value
+        self.__ET_text.visible=True
+        self.__ET_buttons_plain_text.visible=True
+        self.__ET_text_input.visible=False
+        self.__ET_button_done_edit.visible=False
+        self.__ET_text.value=self.__ET_text_input.value
+        self.value=self.__ET_text.value
         self.update()
     def update_value(self,mode:typing.Literal["processing","error","succeed","warn"],input_value:str=""):
         def change_bgcolor(color):
-            self.controls[0].bgcolor=color
+            self.__ET_text.bgcolor=color
         def change_text(input_value:str):
             self.value=input_value
-            self.controls[0].value=input_value
+            self.__ET_text.value=input_value
         match mode:
             case "processing":
                 if input_value=="":
@@ -147,6 +157,7 @@ def _run_papnt_doi(now_text:_Editable_Text):
     if len(response["results"])!=0:
         now_text.update_value("warn","Already added! "+doi)
         return
+    time.sleep(0.1)
     try:
         __create_records_from_doi(doi)
     except:
